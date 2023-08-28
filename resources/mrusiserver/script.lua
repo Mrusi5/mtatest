fb498f95b68e1c0d65c994893adf9e5b4b772ea1
Mrusi5 <113948511+Mrusi5@users.noreply.github.com>
2023-08-28 21:27:42 +0400

    Update script.lua

--- resources/mrusiserver/script.lua
+++ resources/mrusiserver/script.lua
@@ -1,8 +1,5 @@
-local spawnX, spawnY, spawnZ = 1959.55, -1714.46, 10
+
 function joinHandler()
-	spawnPlayer(source, spawnX, spawnY, spawnZ)
-	fadeCamera(source, true)
-	setCameraTarget(source, source)
 	outputChatBox("Hello world!", source)
 end
 addEventHandler("onPlayerJoin", getRootElement(), joinHandler)
